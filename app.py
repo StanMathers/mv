9feeefa5880119dc114b67c777cb64985b40014f
StanMathers <sabagamgebeli@gmail.com>
2022-06-06 22:28:28 +0400

    .

--- app.py
+++ app.py
@@ -4,13 +4,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 res = configuration.Response()
-# print(res.get_top_rated_movies()[0][3])
 
 @app.route('/')
 def index():
     data = res.get_top_rated_movies()
     return render_template('index.html', data = data)
 
+@app.route('/details/<movie_id>')
+def details(movie_id: int):
+    return f'Movie: {movie_id}'
 
 if __name__ == '__main__':
     app.run(debug=True)
