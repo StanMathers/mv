0b253ae3fa6fe4513b1f51582dec7d451b86dc12
StanMathers <sabagamgebeli@gmail.com>
2022-06-07 21:20:35 +0400

    .

--- app.py
+++ app.py
@@ -7,12 +7,17 @@ res = configuration.Response()
 
 @app.route('/')
 def index():
-    data = res.get_top_rated_movies()
-    return render_template('index.html', data = data)
+    top_rated = res.get_top_rated_movies()
+    popular_movies = res.get_popular_movies()
+    return render_template('index.html',
+                           top_rated = top_rated, popular_movies = popular_movies)
 
 @app.route('/details/<movie_id>')
 def details(movie_id: int):
-    return render_template('details.html', movie_id = movie_id)
+    movie_details = res.get_movie_details_by_id(int(movie_id))
+    return render_template('details.html', movie_details = movie_details)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- configuration/config.py
+++ configuration/config.py
@@ -74,9 +74,30 @@ class Response(API, InformationConfiguration):
         for i in data:
             keys.append(i['key'])
         return f"https://www.youtube.com/watch?v={keys[0]}"
+    
+    def get_movie_details_by_id(self, movie_id: int):
+        """
+        Return structure -> list of
+            (poster_url, title, release_date, duration, imdb, overview)
+        """
+        ORIGINAL_TITLE: str = 'original_title'
+        RELEASE_DATE: str = 'release_date'
+        RUNTIME: str = 'runtime'
+        VOTE_AVERAGE = 'vote_average'
+        OVERVIEW: str = 'overview'
+        
+        
+        data_to_return = []
+        
+        response = requests.get(f'https://api.themoviedb.org/3/movie/{movie_id}?api_key={self.KEY}')
+        data = response.json()
+        data_to_return.append((self.IMAGE_BASE_URL + data[self.POSTER_PATH], data[ORIGINAL_TITLE], data[RELEASE_DATE], data[RUNTIME], data[VOTE_AVERAGE],data[OVERVIEW]))
+        return data_to_return
+        
 # 123
 
 if __name__ == '__main__':
     r = Response()
     a = r.get_top_rated_movies()
+    print(r.get_movie_details_by_id(a[0][0]))
 # print(a[0][0])
