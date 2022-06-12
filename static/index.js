5c477833b894def19c21aa48a7f367c572087083
StanMathers <sabagamgebeli@gmail.com>
2022-06-12 21:25:01 +0400

    Idk

---
+++ static/index.js
@@ -0,0 +1,13 @@
+// Dark mode toggle
+// function toggleDarkMode() {
+//     document.body.classList.toggle('dark');
+
+function test(){
+    let a = document.getElementById("checkform");
+    let b = document.getElementById("flexSwitchCheckChecked");
+
+    let mainBody = document.getElementById("main_body");
+    document.body.classList.add("bg-black")
+    mainBody.style.color = 'white'
+
+  }
